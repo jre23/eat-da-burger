ab08c662f0f8dd2c94e5961ac21e2e434bc4775d
Joel Estrada <jre.estrada@gmail.com>
2020-12-04 01:18:20 -0800

    created burger_controller.js and added static and router to server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 // Sets up the Express app
 app.use(express.urlencoded({
     extended: true
@@ -16,6 +15,10 @@ app.engine("handlebars", exphbs({
     defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
