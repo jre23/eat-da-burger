bcacd704da2f381078a7c2595a33351665b896d5
Joel Estrada <jre.estrada@gmail.com>
2020-12-04 00:12:34 -0800

    created schema and seeds mysql files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Quarter Pounder w/ Cheese", "0");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dick's Deluxe", "0");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lil Woody's", "0");
