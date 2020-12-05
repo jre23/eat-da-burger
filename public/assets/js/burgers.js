1de4bdb4e5f1a108d7f0e18eeae6350eaee44463
Joel Estrada <jre.estrada@gmail.com>
2020-12-05 01:06:18 -0800

    edited index.handlebars to display burgers in line, removed console.log from burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,15 +24,12 @@ $(() => {
         let newBurger = {
             burger_name: $("#burg").val().trim(),
         };
-        console.log(newBurger);
-        console.log("test new burger");
         // Send the POST request.
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
         }).then(
             () => {
-                console.log("created new burger");
                 // Reload the page to get the updated list
                 location.reload();
             }
