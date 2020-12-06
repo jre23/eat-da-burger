b3b99bb2a8cc0052e307bf394fc4df6661bdfc9d
Joel Estrada <jre.estrada@gmail.com>
2020-12-05 17:49:49 -0800

    changed const to let for connection variable in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // require dependencies
 const mysql = require("mysql");
 // create connection variable 
-const connection;
+let connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
